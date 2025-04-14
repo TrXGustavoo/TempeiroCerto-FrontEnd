7e407f87de5ed29427ea42452e8ec208640cdd0f
Gustavo Passos <gustavofp111@gmail.com>
2025-04-13 23:31:36 -0300

    Atulizando o navigator

--- src/routes/AppNavigator.tsx
+++ src/routes/AppNavigator.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import CadastroScreen from '../screens/CadastroScreen';
-import Home from '../screens/Home';
 import NovaReceita from '../screens/NovaReceita';
 import HomeScreen from '../screens/HomeScreen';
 
@@ -15,7 +14,6 @@ export default function AppNavigator() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Cadastro" component={CadastroScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
         <Stack.Screen name="NovaReceita" component={NovaReceita} options={{ headerShown: false }}/>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
